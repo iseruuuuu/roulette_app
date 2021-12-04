1cc9b762afef9363f83adb0c4d33ff4738aafaaa
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-04 18:12:55 +0900

    add: 音声を追加

--- lib/screen/home_screen/home_screen_controller.dart
+++ lib/screen/home_screen/home_screen_controller.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
 import 'dart:math' as math;
-
 import '../add_screen/add_screen.dart';
 
 class HomeScreenController extends GetxController {
@@ -9,6 +9,30 @@ class HomeScreenController extends GetxController {
   final item = 0.obs;
   final random = math.Random();
   final resultText = 'aaasss'.obs;
+
+  // AudioCache audioCache = AudioCache();
+  // AudioPlayer advancedPlayer = AudioPlayer();
+  // String? localFilePath;
+  // String? localAudioCacheURI;
+  //
+  // static const SOUND_DATA_UP = 'drum_sound.mp3';
+
+  // final AudioCache _cache = AudioCache(
+  //   fixedPlayer: AudioPlayer(),
+  // );
+
+  final AudioCache _player = AudioCache();
+
+  final items = <String>[
+    'Grogu',
+    'Mace Windu',
+    'Obi-Wan Kenobi',
+    'Han Solo',
+    'Luke Skywalker',
+    'Darth Vader',
+    'Yoda',
+    'Ahsoka Tano',
+  ].obs;
 
   @override
   void onInit() {
@@ -21,8 +45,12 @@ class HomeScreenController extends GetxController {
 
     //乱数の値のリストの文字を探していれる。
 
+    //効果音を入れたい
+    _player.play('drum_sound.mp3');
     //ルーレットで値を入れる
     streamController.add(n);
+
+    //決まったら音がなりたい
   }
 
   void onTapAddScreen() {
