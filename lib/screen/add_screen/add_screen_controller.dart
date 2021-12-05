875d552bf281b8b1d4dbd6163b1362ea195def74
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-05 18:12:42 +0900

    fix: sort indent

--- lib/screen/add_screen/add_screen_controller.dart
+++ lib/screen/add_screen/add_screen_controller.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:roulette_app/model/todo.dart';
 import 'package:roulette_app/preference/preference.dart';
 import 'package:roulette_app/screen/roulette_screen/roulette_screen.dart';
-
 import '../tutorial_screen/tutorial_screen.dart';
 
 class AddScreenController extends GetxController {
