4c6b5e7214b78d1b48184e64bc339ba3a9d44198
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-05 14:01:30 +0900

    add: ガラスのデザインを追加してみた（使わない予定）.

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:get/get.dart';
 import 'package:glassmorphism/glassmorphism.dart';
-import 'package:roulette_app/component/item.dart';
 import 'package:roulette_app/component/item2.dart';
-import 'package:roulette_app/component/roulette_text.dart';
 import 'home_screen_controller.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,14 +11,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(HomeScreenController(), tag: '');
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         height: double.infinity,
         width: double.infinity,
         child: Stack(
           children: [
-            Image.network(
-              "https://github.com/RitickSaha/glassmophism/blob/master/example/assets/bg.png?raw=true",
-              fit: BoxFit.cover,
+            Image.asset(
+              "assets/background2.jpeg",
+              fit: BoxFit.fill,
               height: double.infinity,
               width: double.infinity,
               scale: 1,
@@ -29,37 +26,37 @@ class HomeScreen extends StatelessWidget {
             SafeArea(
               child: Center(
                 child: GlassmorphicContainer(
-                    width: 350,
-                    height: 750,
-                    borderRadius: 20,
-                    blur: 20,
-                    alignment: Alignment.bottomCenter,
-                    border: 2,
-                    linearGradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Color(0xFFffffff).withOpacity(0.1),
-                          Color(0xFFFFFFFF).withOpacity(0.05),
-                        ],
-                        stops: [
-                          0.1,
-                          1,
-                        ]),
-                    borderGradient: LinearGradient(
+                  width: 350,
+                  height: 750,
+                  borderRadius: 20,
+                  blur: 20,
+                  alignment: Alignment.bottomCenter,
+                  border: 2,
+                  linearGradient: LinearGradient(
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
                       colors: [
-                        Color(0xFFffffff).withOpacity(0.5),
-                        Color((0xFFFFFFFF)).withOpacity(0.5),
+                        const Color(0xFFffffff).withOpacity(0.1),
+                        const Color(0xFFFFFFFF).withOpacity(0.05),
                       ],
-                    ),
-                    child: Column(
-                      children: const [
-                        Item2(),
-                        Item2(),
-                      ],
-                    )
+                      stops: const [
+                        0.1,
+                        1,
+                      ]),
+                  borderGradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      const Color(0xFFffffff).withOpacity(0.5),
+                      const Color((0xFFFFFFFF)).withOpacity(0.5),
+                    ],
+                  ),
+                  child: Column(
+                    children: const [
+                      Item2(),
+                      Item2(),
+                    ],
+                  ),
                 ),
               ),
             ),
