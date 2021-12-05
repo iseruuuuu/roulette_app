28a2fbfbaaaf6d3fc381ddd039430120c7563300
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-05 17:30:15 +0900

    add: チュートリアルの条件分岐を追加した

--- lib/screen/add_screen/add_screen_controller.dart
+++ lib/screen/add_screen/add_screen_controller.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:roulette_app/model/todo.dart';
+import 'package:roulette_app/preference/preference.dart';
 import 'package:roulette_app/screen/roulette_screen/roulette_screen.dart';
+
+import '../tutorial_screen/tutorial_screen.dart';
 
 class AddScreenController extends GetxController {
   final _todos = <Todo>[].obs;
   final contents = ''.obs;
+  final isTutorial = false.obs;
 
   @override
   void onInit() {
     super.onInit();
 
-    //TODO チュートリアル画面を出す。
-    onTapShowTutorial();
+    sharedPreference();
+  }
+
+  void sharedPreference() async {
+    print(isTutorial.value);
+    isTutorial.value = await Preference().getBool(PreferenceKey.isTutorial);
+
+    //初回起動のみ
+    if (!isTutorial.value) {
+      showCupertinoModalBottomSheet(
+        expand: true,
+        context: Get.context!,
+        builder: (context) => const TutorialScreen(),
+      );
+    }
+    await Preference().setBool(PreferenceKey.isTutorial, true);
   }
 
 // TODO:フィルタの状態によって返すTodoを変える
@@ -49,7 +68,16 @@ class AddScreenController extends GetxController {
     }
   }
 
-  void onTapShowTutorial() {
-
+  void showAlert(BuildContext context) async {
+    if (isTutorial.value) {
+      showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+          content: Text("hi"),
+        ),
+      );
+      await Preference().setBool(PreferenceKey.isTutorial, false);
+    }
+    await Preference().setBool(PreferenceKey.isTutorial, false);
   }
 }
